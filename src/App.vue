8ef1c0e226f52616a9f11b4d07230c388ae7a188
Mahdi Moazeni <mahdi.moazeni.artisan@gmail.com>
2023-03-23 07:42:42 +0430

    npm install bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
   <h1>idea-processors.ir</h1>
+  <div class="alert alert-info" role="alert">
+    A simple info alert—check it out!
+  </div>
   <router-view></router-view>
 </template>
 
