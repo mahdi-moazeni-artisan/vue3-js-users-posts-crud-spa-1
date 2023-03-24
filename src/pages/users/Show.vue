2c829decbabf5a7e28ff1966d2625a20bb6c36d9
Mahdi Moazeni <mahdi.moazeni.artisan@gmail.com>
2023-03-24 05:56:57 +0430

    add components and router for usersIndex and userShow

---
+++ src/pages/users/Show.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="container mt-2">
+      <h1>User Show Page!</h1>
+      <div class="row mt-3">
+        <div class="col-md-6 border p-1">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus vel a
+            asperiores! Suscipit maxime in dolorem excepturi sequi voluptates,
+            pariatur quas commodi neque quis repudiandae a natus cum, quo non?
+          </p>
+        </div>
+      </div>
+    </div>
+  </template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/router.js
+++ src/router.js
@@ -1,10 +1,12 @@
 import { createRouter,createWebHistory } from "vue-router";
 import Home from './pages/Home.vue'
-import Users from './pages/Users.vue'
+import UsersIndex from './pages/users/Index.vue'
+import UserShow from './pages/users/Show.vue'
 import Posts from './pages/Posts.vue'
 const routes=[
     {path:'/',name:'home',component:Home},
-    {path:'/users',name:'users',component:Users},
+    {path:'/users',name:'usersIndex',component:UsersIndex},
+    {path:'/users/:id',name:'usersShow',component:UserShow},
     {path:'/posts',name:'posts',component:Posts}
 ];
 
